6787f07dd17fcac4447daf7f944ab5cb57c52a02
sagasthy <sivakarthik1990@gmail.com>
2020-11-14 21:38:50 -0500

    first commit

---
+++ src/test/java/com/kar/hellorest/HelloRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kar.hellorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
